9899ff4246e4b726a22a6ab0adf6534bc3e3445c
IvanRusenov <i_rusenov@abv.bg>
2022-12-16 10:04:17 +0200

    Create messages052.java

---
+++ basicSyntaxConditionalStatementsAndLoopsMoreExercise/messages052.java
@@ -0,0 +1,11 @@
+package basicSyntaxConditionalStatementsAndLoopsMoreExercise;
+
+import java.util.Scanner;
+
+public class messages052 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
