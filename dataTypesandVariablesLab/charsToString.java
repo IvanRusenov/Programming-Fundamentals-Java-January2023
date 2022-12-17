23821c2994acd030302506a07e48de71cbfde08f
IvanRusenov <i_rusenov@abv.bg>
2022-12-17 13:04:49 +0200

    Create charsToString.java

---
+++ dataTypesandVariablesLab/charsToString.java
@@ -0,0 +1,15 @@
+package dataTypesandVariablesLab;
+
+import java.util.Scanner;
+
+public class charsToString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        char firstChar = scan.nextLine().charAt(0);
+        char secondChar = scan.nextLine().charAt(0);
+        char thirdChar = scan.nextLine().charAt(0);
+
+        System.out.printf("%c%c%c",firstChar,secondChar,thirdChar);
+    }
+}
