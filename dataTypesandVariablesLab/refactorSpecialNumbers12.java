85956f14cbff33906e2bfd107095c79919402a08
IvanRusenov <i_rusenov@abv.bg>
2022-12-19 13:52:41 +0200

    Create refactorSpecialNumbers12.java

---
+++ dataTypesandVariablesLab/refactorSpecialNumbers12.java
@@ -0,0 +1,25 @@
+package dataTypesandVariablesLab;
+
+import java.util.Scanner;
+
+public class refactorSpecialNumbers12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+            boolean isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+            if (isSpecial){
+                System.out.printf("%d -> True\n", i);
+            }else {
+                System.out.printf("%d -> False\n", i);
+            }
+
+        }
+    }
+}
