9f6d5e60f90dc6826e9d7aee393788bad5db9922
IvanRusenov <i_rusenov@abv.bg>
2022-12-20 18:20:43 +0200

    Create townInfo.java

---
+++ dataTypesandVariablesLab/townInfo.java
@@ -0,0 +1,14 @@
+package dataTypesandVariablesLab;
+
+import java.util.Scanner;
+
+public class townInfo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String town = scan.nextLine();
+        int population = Integer.parseInt(scan.nextLine());
+        int area = Integer.parseInt(scan.nextLine());
+        System.out.printf("Town %s has population of %d and area %d square km.%n",town,population,area);
+    }
+}
